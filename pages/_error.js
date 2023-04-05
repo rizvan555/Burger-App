a6b2083999f9de961068dc16d1500cd5854f21ea
Rizvan <rizvan@MacBook-Air.fritz.box>
2023-04-05 20:35:32 +0200

    added error-page and gave function with useEffect/Timeout after  3 seconds will be go to Homepage

---
+++ pages/_error.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const NextErrorComponent = ({ statusCode }) => {
+  const router = useRouter();
+
+  /** asagidaki UseEffecte setTimeout vermekle Error-page 3 saniye sonra HomePage e kecir */
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="not-found">
+      {statusCode === 404 ? <h1>Page Not Found</h1> : <h1>Error occurred</h1>}
+      <Link href="/" legacyBehavior>
+        <a>Go to Homepage</a>
+      </Link>
+    </div>
+  );
+};
+
+export default function ErrorPage({ statusCode }) {
+  return <NextErrorComponent statusCode={statusCode} />;
+}
